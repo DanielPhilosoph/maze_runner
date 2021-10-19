0e32e678f982a067947121dc8213554b8940bcfe
DanielPhilosoph <danielph9@gmail.com>
2021-10-19 14:17:34 +0300

    async maze - checkpoint
    conver writetofile to async

--- async-module-js.js
+++ async-module-js.js
@@ -1,20 +1,21 @@
+"use strict";
 const fs = require("fs");
 const path = require("path");
-function findTreasureSync(roomPath) {
+function findTreasure(roomPath) {
   drawMapSync(roomPath.toString());
   let mazeArray = fs.readdirSync(roomPath);
   for (let elem of mazeArray) {
     if (elem.split("-")[0] === "chest") {
-      let nextRoom = openChestSync(roomPath + "/" + elem);
-      if (nextRoom === "treasure") {
-        drawMapSync("🏆");
-        return "YAY";
-      }
-      if (nextRoom) {
-        return findTreasureSync(nextRoom);
-      } else {
-        continue;
-      }
+      openChest(roomPath + "/" + elem).then((nextRoom) => {
+        if (nextRoom) {
+          if (nextRoom === "treasure") {
+            drawMapSync("🏆");
+            return;
+          } else {
+            return findTreasure(nextRoom);
+          }
+        }
+      });
     }
   }
 }
@@ -25,36 +26,45 @@ function drawMapSync(currentRoomPath) {
 
 // openChestSync      ==> chestPath = "./maze/room-1/room-0/chest-1.json" <===
 // return "PATH_ROOM"    ==> "./maze/room-1.../room" <==
-function openChestSync(chestPath) {
+function openChest(chestPath) {
   let chestContentJSON = "";
-
-  let chestContent = fs.readFile(chestPath, (err, data) => {
-    if (err) {
+  let y = "";
+  return new Promise((resolve, reject) => {
+    fs.readFile(chestPath, (err, data) => {
+      if (err) {
+        reject();
+      }
+      try {
+        chestContentJSON = JSON.parse(data.toString());
+      } catch (error) {
+        reject();
+      }
+      if (chestContentJSON.hasOwnProperty("treasure")) {
+        resolve("treasure");
+      }
+      validateChestContent(chestContentJSON)
+        .then(() => {
+          resolve(chestContentJSON.clue);
+        })
+        .catch(() => {
+          reject();
+        });
+    });
+  })
+    .then((value) => {
+      return value;
+    })
+    .catch(() => {
       return false;
-    }
-    chestContentJSON = data.toString();
-    if (chestContentJSON.hasOwnProperty("treasure")) {
-      return "treasure";
-    }
-    if (validateChestContent(chestContentJSON)) {
-      return chestContentJSON.clue;
-    } else {
-      return false;
-    }
-  });
+    });
 }
 
 function validateChestContent(chestText) {
-  console.log(JSON.parse(chestText));
-  if (typeof chestText.clue !== "string") {
-    return false;
-  }
-  fs.readdir(chestText.clue, (err, data) => {
-    if (err) {
-      return false;
-    }
-    return true;
+  return new Promise((resolve, reject) => {
+    fs.access(chestText.clue, (err) => {
+      err ? reject() : resolve();
+    });
   });
 }
 
-console.log(findTreasureSync("./maze"));
+console.log(findTreasure("./maze"));
